71283878e9b3b8b4d50b582ec45e7d3acf3c7dff
devguilherme <guilhermedo12345@gmail.com>
2026-01-07 20:34:28 -0300

    Criando Funcionalidads

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,63 @@
+import { useState } from "react"
 
 function App() {
 
   return (
-    <div>Teste</div>
+    <div>
+        <Interface/>
+        <Formulario/>
+    </div>
   )
 }
 
 export default App
+
+function Interface(){
+    return(
+        <div>
+            <img src="src/assets/logo.png"/>
+            <h2>Qual a melhor opção?</h2>
+        </div>
+    )
+}
+
+function Formulario(){
+    const [valorAlcool, setValorAlcool] = useState(0)
+    const [valorGasolina, setValorGasolina] = useState(0)
+    
+    let resultado="";
+
+    function Calcular(){
+        let alcool = valorAlcool
+        let gasolina = valorGasolina
+
+        if(alcool<gasolina){
+            resultado = "Álcool" 
+        }else{
+            resultado = "Gásolina"
+        }
+    }
+    
+
+    return(
+        <div>
+            <label>Álcool (preço por litro):</label>
+            <input type="number"
+            value={valorAlcool}
+            onChange={(e)=> setValorAlcool(Number(e.target.value))}
+            ></input>
+
+            <label>Gasolina (preço por litro):</label>
+            <input type="number"
+            value={valorGasolina}
+            onChange={(e)=>setValorGasolina(Number(e.target.value))}
+            ></input>
+
+            <button onClick={Calcular}>Calcular</button>
+        <div>
+            <h1>Compensa Mais:{resultado}</h1>
+        </div>
+        </div>
+    )
+}
+
